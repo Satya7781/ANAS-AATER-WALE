224604f3759f7f6b75fec5c2df9a9466e505cd22
satya7781 <satyanarayanchohan04@gmail.com>
2026-05-24 09:23:19 +0530

    Add Render startup config

--- app.py
+++ app.py
@@ -1109,4 +1109,5 @@ def inject_globals():
     )
 
 if __name__ == '__main__':
-    app.run(debug=False, host='127.0.0.1')
+    port = int(os.environ.get('PORT', '5000'))
+    app.run(debug=False, host='0.0.0.0', port=port)
